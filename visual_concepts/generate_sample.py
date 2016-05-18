7cfd57338c96b878531749ca2d6cc5916a062561
Vittal <vittalp88@gmail.com>
2016-05-17 21:13:52 -0400

    don't randomize samples

--- visual_concepts/generate_sample.py
+++ visual_concepts/generate_sample.py
@@ -15,9 +15,9 @@ import theano.tensor as T
 
 dcgan_root = "/mnt/disk1/vittal/dcgan_code/visual_concepts/"
 
-desc = "vcgan_l2_multi"
+desc = "vcgan_orig_multi"
 model_dir = dcgan_root + '/models/%s/'%desc
-model_number = "20_gen_params.jl"
+model_number = "25_gen_params.jl"
 gen_params_np = joblib.load(model_dir + model_number)
 gen_params = [sharedX(element) for element in gen_params_np]
 vc_num = 10
@@ -36,7 +36,6 @@ if 'vc_num' in locals():
     data = tr_data.get_data(tr_handle, slice(0, tr_data.num_examples))
     labels = data[labels_idx]
     vc_idx = np.where(labels == vc_num)[0]
-    np.random.shuffle(vc_idx)
     vc_idx = vc_idx[:196]
 
     feat_l2_idx = tr_stream.dataset.provides_sources.index('feat_l2')
