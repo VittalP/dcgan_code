66f7b2888ac72f616e6770ba7a4942a3e7010bec
vittal <vittal@vittal-mac.mtw.jhu.edu>
2016-05-25 18:49:42 -0400

    convert shape to a tuple

--- lib/img_utils.py
+++ lib/img_utils.py
@@ -6,5 +6,5 @@ def transform(X, npx):
     return floatX(X).transpose(0, 3, 1, 2)/127.5 - 1.
 
 def inverse_transform(X, nc, npx):
-    X = (X.reshape(-1, nc, npx, npx).transpose(0, 2, 3, 1)+1.)/2.
+    X = (X.reshape((-1, nc, npx, npx)).transpose(0, 2, 3, 1)+1.)/2.
     return X
