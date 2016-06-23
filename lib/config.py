10c3c779321ea58f794f06429f1d4ba4125fb546
Vittal <vittalp88@gmail.com>
2016-06-23 16:11:26 -0400

    handle marcc case

--- lib/config.py
+++ lib/config.py
@@ -1,2 +1,6 @@
-data_dir = '/mnt/disk1/vittal/visual_concepts/'
-caffe_dir = '/srv/share/code/caffe/'
+import socket
+if 'ccvl' in socket.gethostname():
+    data_dir = '/mnt/disk1/vittal/visual_concepts/'
+    caffe_dir = '/srv/share/code/caffe/'
+else:
+    data_dir = '/home-4/vpremac1@jhu.edu/scratch/'
