e9a9df547e5db9015c6e27aaf98683ec3d57a73c
Vittal <vittalp88@gmail.com>
2016-06-23 16:29:02 -0400

    cv2 is not required

--- lib/vgg.py
+++ lib/vgg.py
@@ -2,7 +2,7 @@ from keras.models import Sequential
 from keras.layers.core import Flatten, Dense, Dropout
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.optimizers import SGD
-import cv2, numpy as np
+import numpy as np
 from sklearn.externals import joblib
 import sys, os
 
